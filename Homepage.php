aa1887f383ac1c5aff4d844bd50a3d26231ff413
AqsaSaleem498 <umersaleem460@gmail.com>
2026-05-16 20:52:48 +0500

    Rename Html page, improve frontend and establish backend conectivity.

---
+++ Homepage.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>PU Proctors Diary</title>
+<link rel="stylesheet" href="Style.css">
+</head>
+<body>
+<div id="page1" class="page active">
+<header>
+<h1>Welcome to PU Proctors Diary</h1>
+<p>Manage exam duties, attendance, reports, leaves and payments all in one place</p>
+<div class="btn-group">
+<button onclick="location.href='Login.php'">Login</button>
+<button onclick="location.href='Register.php'">Register</button>
+</div>
+</header>
+<div class="container">
+<div class="features">
+<div class="card">
+<img src="calender.jpeg">
+<h3>Duty Assignment</h3>
+<p>Admins assign exam duties based on availability and center preferences.</p>
+</div>
+<div class="card">
+<img src="tick.jpeg">
+<h3>Attendance & Reports</h3>
+<p>Invigilators mark attendance and supervisors upload daily reports.<p>
+</div>
+<div class="card">
+<img src="money.jpeg">
+<h3>Payments</h3>
+<p>Verified duties automatically generate payment summaries.</p>
+</div>
+<div class="card">
+<img src="leave.jpeg">
+<h3>Leave & Availability</h3>
+<p>Staff can apply for leaves or update availability to avoid unwanted assignments.</p>
+</div>
+<div class="card">
+<img src="dashboard.jpeg">
+<h3>Role-Based Dashboards</h3>
+<p>Each user gets a tailored dashboard for focused operations.</p>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
